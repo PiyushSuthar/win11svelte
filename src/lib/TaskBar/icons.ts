570d6f6597e2f5c536a2513ed0fe64777f3fad9f
Piyush Suthar <piyushsuthar100@gmail.com>
2023-08-18 16:41:37 +0530

    feat(taskbar): added assets

---
+++ src/lib/TaskBar/icons.ts
@@ -0,0 +1,19 @@
+import StartMenuIcon from "../../assets/taskbar/Windows.png"
+import WidgetsIcon from "../../assets/taskbar/Widgets.png"
+import FileExplorerIcon from "../../assets/taskbar/file_explorer.webp"
+import TaskViewIcon from "../../assets/taskbar/taskview.png"
+import SearchIcon from "../../assets/taskbar/search-light.svg"
+import SearchIconDark from "../../assets/taskbar/search-dark.svg"
+import LightModeIcon from "../../assets/taskbar/light_mode.png"
+import DarkModeIcon from "../../assets/taskbar/dark_mode.png"
+
+export default {
+    StartMenuIcon,
+    WidgetsIcon,
+    FileExplorerIcon,
+    TaskViewIcon,
+    SearchIcon,
+    SearchIconDark,
+    LightModeIcon,
+    DarkModeIcon
+}
